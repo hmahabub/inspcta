c38c3af9f3999eee12d3f1f15833c53a382418aa
hmahabub <hm.sayem.14@gmail.com>
2025-05-03 13:57:37 +0600

    2nd commit

--- inspecta/settings.py
+++ inspecta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-*43&i40j+prj1*m*3jea83vgpf@cyj7ifv8tvqlz7511rseia0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
