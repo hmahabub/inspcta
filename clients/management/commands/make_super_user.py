7a5365f702b4c936c792f2d26308fa4fccbb27f6
hmahabub <hm.sayem.14@gmail.com>
2025-05-03 14:35:35 +0600

    5th commit

--- clients/management/commands/make_super_user.py
+++ clients/management/commands/make_super_user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
+from django.core.management.base import BaseCommand
 
-if not User.objects.filter(username='inspcta_erp_admin').exists():
-    User.objects.create_superuser('inspcta_erp_admin', 'hm.sayem.14@gmail.com', 'adminpass23')
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='inspcta_erp_admin').exists():
+            User.objects.create_superuser('inspcta_erp_admin', 'hm.sayem.14@gmail.com', 'adminpass23')
