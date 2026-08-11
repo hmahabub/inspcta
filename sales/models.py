23d5268037d50d2d1c5020a05d2426b044276f68
hmahabub <hm.sayem.14@gmail.com>
2026-08-11 07:55:58 -0700

    float decimal mismatch fix

--- sales/models.py
+++ sales/models.py
@@ -55,11 +55,11 @@ class Sale(models.Model):
 
     @property
     def net_recieved(self):
-        return self.recieved_amount - self.vat
+        return float(self.recieved_amount) - self.vat
 
     @property
     def balance_amount(self):
-        return self.total_amount - self.recieved_amount
+        return float(self.total_amount) - self.recieved_amount
 
     @property
     def bdt_equivalent(self):
