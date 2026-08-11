327d55111298f7452fa37e30813adda31dc9b501
hmahabub <hm.sayem.14@gmail.com>
2026-08-11 08:17:08 -0700

    float decimal mismatch fix6

--- sales/models.py
+++ sales/models.py
@@ -52,7 +52,8 @@ class Sale(models.Model):
         if self.project.client.types == 'INT':
             vat_amount = Decimal('0.00')
         else:
-            vat_amount = Decimal(self.total_amount) *  Decimal('0.15')
+            vat_rate = Decimal("0.15")
+            vat_amount = Decimal(self.total_amount) *  vat_rate
         return vat_amount
 
     @property
